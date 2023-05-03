08e73efa20c11d30d41ad567736dd427e5c40ad6
LexVulgarity <chromweechannelusa@gmail.ru>
2023-05-03 13:58:40 +0300

    Branch Update

--- Recipies/main/models.py
+++ Recipies/main/models.py
@@ -5,6 +5,7 @@ class Recipe(models.Model):
     title = models.CharField('Название', max_length=50)
     describe = models.TextField('Описание')
     image = models.ImageField(upload_to='img/',blank=True)
+    rec_time=models.DateTimeField(auto_now_add=True)
     objects = models.Manager()
 
 
