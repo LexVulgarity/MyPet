70fa32f6ac8b44a478d443cd9b63f31afe914363
LexVulgarity <chromweechannelusa@gmail.com>
2023-05-08 16:30:29 +0300

    Внедрил авторизацию

--- Recipies/Recipies/settings.py
+++ Recipies/Recipies/settings.py
@@ -28,6 +28,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+AUTH_USER_MODEL='users.User'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 # Application definition
 
@@ -40,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
+    'users',
 ]
 
 MIDDLEWARE = [
